3953addf0e41e77249d7fdc6d8f581c81a9d701d
Emanuele Ianni <emanuele.ianni@rumbo.com>
2014-06-11 17:23:33 +0200

    Initial commit

---
+++ src/java/test/com/bravofly/exercise/main/MainTest.java
@@ -0,0 +1,14 @@
+package com.bravofly.exercise.main;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MainTest
+{
+  @Test
+  public void iCanCreateAnEdge() {
+    assertThat(true,is(Boolean.TRUE));
+  }
+}
