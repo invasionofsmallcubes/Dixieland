4a0fdf75b6f350e95c8bcb70fde859ce47c980c2
invasionofsmallcubes <dierre@gmail.com>
2014-06-12 21:03:29 +0200

    added scenario 5

--- src/java/test/com/bravofly/exercise/main/TravelTimeTest.java
+++ src/java/test/com/bravofly/exercise/main/TravelTimeTest.java
@@ -14,45 +14,62 @@ import static org.junit.Assert.assertThat;
 public class TravelTimeTest {
 
     private Routes routes;
-    private  List<Airports> itinerary = new ArrayList<>();;
+    private List<Airports> itinerary = new ArrayList<>();
 
     @Before
     public void setUp() throws Exception {
 
-        Set<Flight> flights = new HashSet<>(1);
-        flights.add(new Flight(Airports.Magenta,Airports.Navy,5));
-        flights.add(new Flight(Airports.Navy,Airports.Orange,6));
+        Set<Flight> flights = new HashSet<>();
 
+        flights.add(new Flight(Airports.Magenta, Airports.Navy, 5));
+        flights.add(new Flight(Airports.Navy, Airports.Orange, 4));
+        flights.add(new Flight(Airports.Orange, Airports.Pink, 8));
+        flights.add(new Flight(Airports.Pink, Airports.Orange, 8));
+        flights.add(new Flight(Airports.Pink, Airports.Quartz, 6));
+        flights.add(new Flight(Airports.Magenta, Airports.Pink, 5));
+        flights.add(new Flight(Airports.Orange, Airports.Quartz, 2));
+        flights.add(new Flight(Airports.Quartz, Airports.Navy, 3));
+        flights.add(new Flight(Airports.Magenta, Airports.Quartz, 7));
 
         routes = new Routes(flights);
     }
 
     @Test
-    public void testATravelTimeForOneItineraryWithTwoAirports() {
-        itinerary.add(Airports.Magenta);
-        itinerary.add(Airports.Navy);
-        assertTravelTime(itinerary, 5);
+    public void scenarioOne() {
+        givenTheItinerary(Airports.Magenta, Airports.Navy, Airports.Orange);
+        thenICanAssertTravelTimeOf(9);
     }
 
     @Test
-    public void testATravelTimeForOneItineraryWithTwoAnotherAirports() {
-        itinerary.add(Airports.Navy);
-        itinerary.add(Airports.Orange);
-        assertTravelTime(itinerary, 6);
+    public void scenarioTwo() {
+        givenTheItinerary(Airports.Magenta, Airports.Pink);
+        thenICanAssertTravelTimeOf(5);
     }
 
     @Test
-    public void testATravelTimeForWithAChange() {
-        itinerary.add(Airports.Magenta);
-        itinerary.add(Airports.Navy);
-        itinerary.add(Airports.Orange);
-
-        assertTravelTime(itinerary,11) ;
+    public void scenarioThree() {
+        givenTheItinerary(Airports.Magenta, Airports.Pink, Airports.Orange);
+        thenICanAssertTravelTimeOf(13);
     }
 
-    private void assertTravelTime(List<Airports> itinerary, int expectedTravelTime) {
+    @Test
+    public void scenarioFour() {
+        givenTheItinerary(Airports.Magenta,Airports.Quartz,Airports.Navy,Airports.Orange,Airports.Pink);
+        thenICanAssertTravelTimeOf(22);
+    }
+
+    @Test
+    public void scenarioFive() {
+        givenTheItinerary(Airports.Magenta,Airports.Quartz,Airports.Pink);
+        thenICanAssertTravelTimeOf(0);
+    }
+
+    private void givenTheItinerary(Airports... airports) {
+        for (Airports a : airports) itinerary.add(a);
+    }
+
+    private void thenICanAssertTravelTimeOf(int expectedTravelTime) {
         assertThat(routes.getTravelTime(itinerary), is(expectedTravelTime));
     }
-
 
 }
